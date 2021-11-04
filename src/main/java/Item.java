3da9ea3a970ff29a9f589b5f42a11da31b14ec0d
Carlos <carlostena@centroafuera.onmicrosoft.com>
2021-11-04 11:43:24 +0100

    Initial commit

---
+++ src/main/java/Item.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class Item {
+
+    public String nombre;
+    public int precio;
+
+    Item(){
+        Random r = new Random();
+        nombre = "Item"+r.nextInt();
+        precio = r.nextInt(100) + 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
